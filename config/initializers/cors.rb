f5e3ba8d6d0dbe807bb01be42a637fe924ec4785
Purnama S Rahayu <purnamasrahayu@gmail.com>
2024-08-28 20:01:09 +0700

    edit cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'https://flowerize.vercel.app/', 'http://flowerize.vercel.app/', 'http://localhost:3001' # Replace with the domain of your frontend application
+      origins 'https://flowerize.vercel.app', 'http://flowerize.vercel.app', 'http://localhost:3001' # Replace with the domain of your frontend application
   
       resource '*',
         headers: :any,
